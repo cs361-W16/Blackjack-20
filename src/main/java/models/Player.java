ba207167b6289414203db81e0c4b4995a0367c40
dominic <giacoppd@oregonstate.edu>
2016-03-10 23:29:58 -0800

    Fixed a bug in split, now you can't split whenever you want to

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -18,6 +18,21 @@ public class Player
         isSplit = false;
     }
 
+    public void split(Game g){
+        if(g.cols.get(1).get(0).getValue() == g.cols.get(1).get(1).getValue()) {//if card 1 value == card 2
+            g.move(1, 2); //moves one of the cards over to the new stack
+            this.isSplit = true;
+            //TODO BETTING FUNCTION GOES HERE
+        }
+
+    }
+
+    public void doubleDown(int i, Game g){
+        bet();
+        g.hit(i); //TODO ACTUALLY IMPLEMENT THIS I THINK IT"S WRONG ALSO CHECK TO MAKE SURE YOU CAN DO IT
+        //stay();
+    }
+
     public void bet()
     {
         if (money >= betAmount) {
@@ -34,7 +49,9 @@ public class Player
         int dealScore = g.getColScore(0);
         while(dealScore <= 16) { //dealer keeps hitting
             g.hit(0);
+            dealScore = g.getColScore(0);
         }
         return;
+        //TODO NEEDS LEYGIT GAME SCORING AND ENDING
         }
 }
